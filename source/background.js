832759a2ed0ab653efd8087e2cf9bb1affc10d64
Anton Shchekota <anton.redfox@gmail.com>
2021-05-12 22:06:58 +0300

    Initial commit of implementation

--- source/background.js
+++ source/background.js
@@ -1,2 +1,92 @@
-// eslint-disable-next-line import/no-unassigned-import
-import './options-storage.js';
+import browser from "webextension-polyfill";
+
+function parseStartItem(startItem) {
+	const [itemTypeName, itemID, versionModificator] = (startItem || "").split(
+		":"
+	);
+	if (itemTypeName && itemID) {
+		return {
+			itemTypeName,
+			itemID,
+			versionModificator
+		};
+	}
+
+	return null;
+}
+
+async function switchOnActiveTab(tab) {
+	const urlString = tab.url;
+	if (!(urlString && urlString.startsWith("http"))) {
+		return;
+	}
+
+	const url = new URL(urlString);
+	url.search = url.search.toLowerCase();
+	const startItem = url.searchParams.get("startitem");
+	const dataItem = parseStartItem(startItem);
+	if (!dataItem) {
+		return;
+	}
+
+	url.search = "";
+	console.log(url.toString() + "?*");
+	const tabs = await browser.tabs.query({
+		url: [url.toString(), url.toString() + "?*"],
+		active: false
+	});
+	if (tabs.length === 0) {
+		return;
+	}
+
+	const active = tabs[0];
+	try {
+		await browser.tabs.executeScript(tab.id, {
+			code: `localStorage.removeItem("DeepLinkingOpenStartItem");localStorage.setItem("DeepLinkingOpenStartItem", '${JSON.stringify(
+				dataItem
+			)}');`
+		});
+		await browser.tabs.executeScript(tab.id, {
+			code: "window.stop();",
+			allFrames: true,
+			runAt: "document_start"
+		});
+		await browser.tabs.update(active.id, { active: true });
+		await browser.tabs.remove(tab.id);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+const isFirefox = (window.browser && browser.runtime) || !chrome.app;
+if (!isFirefox) {
+	browser.tabs.onUpdated.addListener(async (tabId, { url }, tab) => {
+		if (url) {
+			await switchOnActiveTab(tab);
+		}
+	});
+} else if (isFirefox) {
+	browser.runtime.getBrowserInfo().then((browserInfo) => {
+		if (Number.parseInt(browserInfo.version, 10) < 88) {
+			browser.tabs.onUpdated.addListener(
+				async (tabId, { url }, tab) => {
+					if (url) {
+						await switchOnActiveTab(tab);
+					}
+				},
+				{
+					properties: ["status"]
+				}
+			);
+		} else {
+			browser.tabs.onUpdated.addListener(
+				async (tabId, changeInfo, tab) => {
+					await switchOnActiveTab(tab);
+				},
+				{
+					properties: ["url"]
+				}
+			);
+		}
+	});
+}
